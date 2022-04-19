1e0c9f4448229e61505cb440c9878eb1c2c18012
moez-rd <moez.muhardin@gmail.com>
2022-04-19 08:52:43 +0700

    Add auth

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
