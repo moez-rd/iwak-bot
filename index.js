95219150ad7ecb357b9cf3c13c3497e23124a866
moez-rd <moez.muhardin@gmail.com>
2022-04-19 09:50:12 +0700

    Add features

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 
 const client = new Client({
 	authStrategy: new LocalAuth()
@@ -24,5 +24,42 @@ client.on('message', message => {
 		message.reply('Hi juga');
 	}
 });
+
+client.on('message', async (msg) => {
+	const chat = await msg.getChat();
+	const mentions = await msg.getMentions();
+
+	for (let contact of mentions) {
+		chat.sendMessage(`@${contact.id.user}, dirimu dipanggil`, {
+			mentions: [contact]
+		});
+	}
+});
+
+client.on('message', async (msg) => {
+	if (msg.body === '!everyone') {
+		const chat = await msg.getChat();
+
+		let text = "";
+		let mentions = [];
+
+		for (let participant of chat.participants) {
+			const contact = await client.getContactById(participant.id._serialized);
+
+			mentions.push(contact);
+			text += `@${participant.id.user} `;
+		}
+
+		await chat.sendMessage(text, { mentions });
+	}
+});
+
+client.on('message', async (msg) => {
+	if (msg.body === '!pagy') {
+		const chat = await msg.getChat();
+		const media = MessageMedia.fromFilePath('./assets/pagy.jpg');
+		chat.sendMessage(media);
+	}
+})
 
 client.initialize();
