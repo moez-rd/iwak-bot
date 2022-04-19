000d6c5c0d4c17644851ae8495662235ec8f9339
moez-rd <moez.muhardin@gmail.com>
2022-04-19 09:22:16 +0700

    Add auth

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+	authStrategy: new LocalAuth()
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+	qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+	console.log('Client is ready!');
 });
 
 client.on('message', message => {
-	if(message.body === '!ping') {
+	if (message.body === '!ping') {
 		message.reply('pong');
+	}
+});
+
+client.on('message', message => {
+	if (message.body === '!hi') {
+		message.reply('Hi juga');
 	}
 });
 
